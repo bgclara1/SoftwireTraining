7f22e7c77ef40f1d6a6474e6eade27b744ca7355
Clara BG <clara.bollengandolfo@gmail.com>
2025-07-01 15:36:59 +0100

    part 1 and 2.1 done

--- app/src/main/java/com/example/fizzbuzz/main.kt
+++ app/src/main/java/com/example/fizzbuzz/main.kt
@@ -1,5 +1,37 @@
 package com.example.fizzbuzz
 
 fun main() {
-    println("Hello world!")
+
+    val sevens = "Bang"
+  //  println("Hello world!")
+    for (i in 1..100) {
+        var output  = ""
+        var normal = false
+        if (i%3==0 && i%5==0){
+            output = "FizzBuzz"
+        //    println("FizzBuzz")
+        } else if (i % 3 == 0) {
+            output = "Fizz"
+       //     println("Fizz")
+        } else if (i%5==0) {
+            output = "Buzz"
+         //   println("Buzz")
+        }
+        else {
+            output = i.toString()
+            normal = true
+          //  println(i)
+        }
+
+        if (i%7==0) {
+            if (normal == false) {
+                output = "$output$sevens"
+            } else {
+                output = sevens
+            }
+        }
+        println(output)
+    }
+
+
 }
